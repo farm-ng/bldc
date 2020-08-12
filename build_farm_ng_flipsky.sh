a95ab91ead4a6700c42fd6579c48767100375fc4
Ethan Rublee <ethan.rublee@gmail.com>
2020-08-12 12:30:03 -0700

    Add some build scripts.

--- build_farm_ng_flipsky.sh
+++ build_farm_ng_flipsky.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -ex
 
-#make -j7 build_args='-DHW_SOURCE=\"hw_60.c\" -DHW_HEADER=\"hw_60.h\" -DHW60_IS_MK4 -DAPP_CUSTOM_TO_USE=\"app_farm_ng.c\" ' USE_VERBOSE_COMPILE=no
 make -j7 build_args='-DHW_SOURCE=\"hw_60.c\" -DHW_HEADER=\"hw_60.h\" -DAPP_CUSTOM_TO_USE=\"app_farm_ng.c\" ' USE_VERBOSE_COMPILE=no
---
+++ build_farm_ng_trampa.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -ex
+
+make -j7 build_args='-DHW_SOURCE=\"hw_60.c\" -DHW_HEADER=\"hw_60.h\" -DHW60_IS_MK4 -DAPP_CUSTOM_TO_USE=\"app_farm_ng.c\" ' USE_VERBOSE_COMPILE=no
+
